460ecef5864154695b28307af65f006b5fc373ef
Mario <mario@die-davids.de>
2018-07-05 19:37:10 +0200

    removed example customer

--- modules/web/src/de/diedavids/cuba/taggable/web/example/customer/CustomerEdit.java
+++
@@ -1,9 +0,0 @@
-package de.diedavids.cuba.taggable.web.example.customer;
-
-/*
-import com.haulmont.cuba.gui.components.AbstractEditor;
-import de.diedavids.cuba.taggable.entity.example.Customer;
-
-public class CustomerEdit extends AbstractEditor<Customer> {
-}
-*/
