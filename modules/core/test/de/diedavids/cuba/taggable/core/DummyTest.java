c1309f03dddb3002580afea123ff98778e9be626
Mario <mario@die-davids.de>
2018-07-05 20:19:59 +0200

    fixed tests

---
+++ modules/core/test/de/diedavids/cuba/taggable/core/DummyTest.java
@@ -0,0 +1,28 @@
+package de.diedavids.cuba.taggable.core;
+
+import de.diedavids.cuba.taggable.DdctTestContainer;
+import com.haulmont.cuba.core.EntityManager;
+import com.haulmont.cuba.core.Persistence;
+import com.haulmont.cuba.core.Transaction;
+import com.haulmont.cuba.core.TypedQuery;
+import com.haulmont.cuba.core.global.AppBeans;
+import com.haulmont.cuba.core.global.DataManager;
+import com.haulmont.cuba.core.global.Metadata;
+import com.haulmont.cuba.security.entity.User;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class DummyTest {
+
+    @Test
+    public void testLoadUser() {
+        assertTrue(true);
+    }
+}
