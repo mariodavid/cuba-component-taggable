68163421af1b7637f5a127b7d62a49f4f6f55b99
Mario David <mario@die-davids.de>
2018-07-15 20:50:29 +0200

    added postgres support (#1)

---
+++ modules/core/db/init/postgres/10.create-db.sql
@@ -0,0 +1,35 @@
+-- begin DDCT_TAG
+create table DDCT_TAG (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    VALUE_ varchar(255) not null,
+    --
+    primary key (ID)
+)^
+-- end DDCT_TAG
+-- begin DDCT_TAGGING
+create table DDCT_TAGGING (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    TAG_ID uuid not null,
+    TAGGABLE varchar(255) not null,
+    TAGGER_ID uuid,
+    CONTEXT varchar(255),
+    --
+    primary key (ID)
+)^
+-- end DDCT_TAGGING
